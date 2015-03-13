4379492d119178aac110bd8c76684a7ec9c00a3e
Holger Berger <holger.berger@googlemail.com>
2015-03-13 20:40:44 +0100

    added license

--- src/ws.h
+++ src/ws.h
@@ -1,6 +1,38 @@
 #ifndef WS_H
 #define WS_H
 
+/*
+ *    workspace++
+ *
+ *    c++ version of workspace utility
+ *  a workspace is a temporary directory created in behalf of a user with a limited lifetime.
+ *  This version is not DB and configuration compatible with the older version, the DB and 
+ *    configuration was changed to YAML files.
+ * 
+ *  differences to old workspace version
+ *    - usage of YAML file format
+ *    - using setuid or capabilities (needs support by filesystem!)
+ *    - always moves released workspace away (this change is affecting the user!)
+ *
+ *  (c) Holger Berger 2013, 2014, 2015
+ * 
+ *  workspace++ is based on workspace by Holger Berger, Thomas Beisel and Martin Hecht
+ *
+ *  workspace++ is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  workspace++ is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with workspace++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 // C++ stuff
 #include <string>
 #include <vector>
