e09ac267f6d9287ce1b961fba8c732901456596e
Holger Berger <holger.berger@googlemail.com>
2016-12-09 16:26:05 +0100

    discard output of clean + prep

--- testing/prepare_and_run.sh
+++ testing/prepare_and_run.sh
@@ -1,5 +1,6 @@
 # be carefull, this cleans, prepares and runs all tests
+# discards all output of clean and prepare, use only if setups is proven to work
 
-scripts/cleanup_tests.sh
-scripts/prepare_tests.sh
+scripts/cleanup_tests.sh 2>/dev/null >/dev/null
+scripts/prepare_tests.sh 2>/dev/null >/dev/null
 ./run_tests.sh
