fd962789bb269be4348593eaf2f18f8e463008c0
Michael Müller <michael.mueller12@tu-dresden.de>
2023-11-13 15:54:26 +0100

    upped version and uncommented installation

--- getyamlcpp.sh
+++ getyamlcpp.sh
@@ -12,7 +12,7 @@ tar xzf yaml-cpp-0.6.3.tar.gz
 #wget https://yaml-cpp.googlecode.com/files/yaml-cpp-0.5.0.tar.gz
 #tar xzf yaml-cpp-0.5.0.tar.gz
 
-#cd yaml-cpp-0.5.0
-#cmake -D CMAKE_INSTALL_PREFIX=$PWD/.. .
-#make -j
-#make install
+cd yaml-cpp-0.6.3
+cmake -D CMAKE_INSTALL_PREFIX=$PWD/.. .
+make -j
+make install
